f380fb18b03833da075ed803725ee2919fc72dfc
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-04-11 22:33:33 +0200

    paste dc ok

--- tschanger.sh
+++ tschanger.sh
@@ -25,7 +25,24 @@ function copy {
 }
 
 function pastedc {
-  echo "this is pastedc:"
+  dc_old="$(powershell.exe -Command '(Get-Item '$1').CreationTime.ToString("yyyy-MM-dd HH:mm:ss")')"
+  dm_old="$(powershell.exe -Command '(Get-Item '$1').LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss")')"
+  dc_new=$(sed -n '1p' "$clip_file")
+  dm_new=$(sed -n '2p' "$clip_file")
+  echo "File:         $1"
+  echo "DateCreated:  $dc_old (old)"
+  echo "DateCreated:  $dc_new (new)"
+  echo "DateModified: $dm_old (old)"
+  echo "DateModified: $dm_new (new)"
+
+  read -p "Apply changes? (y/n) " yn
+  if [ "${yn,,}" = "y" ]
+  then
+    powershell.exe -Command "(Get-Item '$1').CreationTime=[datetime]::ParseExact('$dc_new', 'yyyy-MM-dd HH:mm:ss', \$null)"
+    echo "Done"
+  else
+    echo "Cancel"
+  fi
 }
 
 function pastedm {
