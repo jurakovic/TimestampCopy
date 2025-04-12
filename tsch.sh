dd23803663e660827d6ce3b33ace238a9ae6ff1c
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-04-12 20:39:27 +0200

    merge files and rename to tsch

--- tsch.sh
+++ tsch.sh
@@ -1,17 +1,117 @@
 #!/bin/bash
 
+##### constants
+
+bashPath="C:\Program Files\Git\usr\bin\bash.exe"
+scriptPath="$(cygpath -w "$(pwd)")\tsch.sh"
+iconPath="$(cygpath -w "$(pwd)")\tsch.ico"
+
+fRootKey='HKEY_CLASSES_ROOT\*\shell\TimestampChanger'
+dRootKey='HKEY_CLASSES_ROOT\Directory\shell\TimestampChanger'
+
 clip_file="$HOME/.tsch"
 datetime_format="yyyy-MM-dd HH:mm:ss"
 
-function main {
-  #echo "number of args: $#"
-  #echo "func: $1"
-  #echo "file: $2"
+function main() {
+  if [ "$#" -eq 1 ] # for install/uninstall
+  then
+    case $1 in
+      "-i") install ;;
+      "-u") uninstall ;;
+    esac
+  elif [ "$#" -eq 2 ] # for copy/paste operations
+  then
+    $1 "$2"
+    __pause
+  else
+    show_menu
+  fi
+}
 
-  $1 "$2"
+##### install/uninstall functions
 
+function show_menu() {
+  set +e
+  clear
+  echo
+  echo -e "Timestamp Changer"
+  echo "                            "
+  echo "  [i] Install               "
+  echo "  [u] Uninstall             "
+  echo "                            "
+  echo "  [q] Quit                  "
+  echo
+  read -p "Choose option: " option
+  clear
+  __perform_action $option
+  if [ $option != "q" ] # else "quit"
+  then
+    __pause
+    show_menu
+  fi
+}
+
+function __perform_action() {
+  case $1 in
+    "i") install ;;
+    "u") uninstall ;;
+    "q") ;; # do nothing, will quit
+    *)   echo "unknown option: $1" ;;
+  esac
+}
+
+function __pause() {
   read -p "Press any key to continue..." -n1 -s; echo
 }
+
+function install() {
+  install_internal "$fRootKey"
+  install_internal "$dRootKey"
+  echo "Install done"
+}
+
+function install_internal() {
+  itemPath="$1\\shell"
+  add_menu_root "$1" "Timestamp Changer" "$iconPath"
+  #add_menu_item "$itemPath\\010Backup" "Backup" "backup"
+  #add_menu_item "$itemPath\\020Restore" "Restore" "restore"
+  #add_item_sep  "$itemPath\\020Restore"
+  add_menu_item "$itemPath\\030CopyTimestamps" "Copy Timestamps" "copy"
+  add_menu_item "$itemPath\\040PasteDateCreated" "Paste DateCreated" "pastedc"
+  add_menu_item "$itemPath\\050PasteDateModified" "Paste DateModified" "pastedm"
+  add_menu_item "$itemPath\\060PasteDateCreatedModified" "Paste DateCreated and Modified" "pastedcdm"
+}
+
+function add_menu_root() {
+  reg.exe add "$1" -v MUIVerb -d "$2" -f
+  reg.exe add "$1" -v SubCommands -d "" -f
+  reg.exe add "$1" -v Icon -d "$3" -f
+}
+
+function add_menu_item() {
+  # key, label, arg
+  reg.exe add "$1" -ve -d "$2" -f
+  reg.exe add "$1\\command" -ve -d "\"$bashPath\" --login -i \"$scriptPath\" \"$3\" \"%1\"" -f
+}
+
+function add_item_sep() {
+  reg.exe add "$1" -v CommandFlags -t REG_DWORD -d 0x40 -f # separator
+}
+
+function uninstall() {
+  uninstall_internal "$fRootKey"
+  uninstall_internal "$dRootKey"
+  echo "Uninstall done"
+}
+
+function uninstall_internal() {
+  if reg.exe query "$1" > /dev/null 2>&1; then
+    echo "y" | reg.exe delete "$1" > /dev/null 2>&1
+    echo "deleted $fRootKey"
+  fi
+}
+
+##### copy/paste functions
 
 function copy {
   dc="$(powershell.exe -Command '(Get-Item '\"$1\"').CreationTime.ToString('\"$datetime_format\"')')"
